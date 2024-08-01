f12f62968f3ecbece19e7fd63b3f1c8e7961d6b3
PugalotlALT <aidan.prior2@education.nsw.gov.au>
2024-08-01 11:39:59 +1000

    commenting and attempting sprites

--- Examples/BlueScreen.asm
+++ Examples/BlueScreen.asm
@@ -13,24 +13,24 @@ SetThumbMode:
 
 Program:
 	ldr r0,StackAddress
-	mov sp, r0
+	mov sp, r0				;Sets up the stack (unused)
 
 	ldr r0, DisplayAddress
 	ldr r1, Mode3BG2
-	str r1, [r0,#0]
+	str r1, [r0,#0]			;Sets the GBA screen mode to Mode 3 (16bit colour bitmap) on Background 2
 	
-	ldr r0, VRAMAddress
-	ldr r1, Blue
-	ldr r2, CanvasSize
+	ldr r0, VRAMAddress		;The address where the data will be written
+	ldr r1, Blue			;The colour to write
+	ldr r2, CanvasSize		;The amount of writes to fill the canvas
 	
 Loop1:
-	strh r1,[r0,#0]
-	add r0, r0, #2
-	sub r2, r2, #1
-	bne Loop1
+	strh r1,[r0,#0]			;Store the colour at the VRAM address
+	add r0, r0, #2			;Add two to the VRAM address (16bit)
+	sub r2, r2, #1			;Decrement the amount of writes
+	bne Loop1				;If >0, loop
 
 Infinite:
-	b Infinite
+	b Infinite				;Loops forever
 	
 	.align 4
 DisplayAddress:
