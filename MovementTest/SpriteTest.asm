531994b7b0f5b9c21b21d0b31073d0ff15085120
PugalotlALT <aidan.prior2@education.nsw.gov.au>
2024-08-03 15:16:40 +1000

    working background

--- MovementTest/SpriteTest.asm
+++ MovementTest/SpriteTest.asm
@@ -3,7 +3,7 @@
 b SetThumbMode
 
 .incbin "./header.bin"
-	
+
 SetThumbMode:
 	adr r0,Program		;Loads the program address
 	add r0,r0,#1		;Adds one to set the first bit to 1 (enter THUMB mode)
@@ -13,17 +13,20 @@ SetThumbMode:
 
 Program:
 	bl Mode0Setup			;Sets up sprites and tiles (see Mode0Setup.asm or SpriteTest.asm in BasicExamples)
+	mov r1, #0b00000000
+	mov r2, #0b00000000
+	
+SetSpriteMovement:
+	bl DPADMovement
 	
 WaitForVBlank:
 	ldr r0, VBLankAddress
-	ldrh r1, [r0,#0]
-	cmp r1, #161
+	ldrh r4, [r0,#0]
+	cmp r4, #161
 	bne WaitForVBlank
 	
 	mov r0, #0					;Address object 1
-	mov r1, #0b00001000		;Attribute 0, sets Y-position
-	ldr r2, TwoByTwoSprite	;Sets sprite mode to 16x16 (high 8 bits)
-	mov r3, #0b11100000		;Sets X-position (low 8 bits)
+	ldr r3, TwoByTwoSprite	;Sets sprite mode to 16x16 (high 8 bits)
 	orr r2, r3				;Attribute 1, combines the sprite mode and position
 	mov r3, #0b00000001		;Attribute 2, sets tile type
 	bl SetSprite
@@ -31,4 +34,33 @@ WaitForVBlank:
 Infinite:
 	b Infinite				;Loops forever
 	
+DPADMovement:			;Takes the X-value from r2 and the Y-value from r1 and increments / decrenemts them based on the D-Pad
+	push {r0, r3, lr}
+	ldr r0, ButtonAddress
+	ldrh r0, [r0, #0]
+	mov r3, #0b1000000
+	tst r0, r3
+	beq SkipUp
+	sub r1, r1, #1
+SkipUp:
+	lsl r3, r3, #1
+	tst r0, r3
+	beq SkipDown
+	add r1, r1, #1
+SkipDown:
+	lsl r3, r3, #1
+	tst r0, r3
+	beq SkipDown
+	add r2, r2, #1
+SkipRight:
+	lsl r3, r3, #1
+	tst r0, r3
+	beq SkipDown
+	sub r2, r2, #1
+SkipLeft:
+	pop {r0, r3, pc}
+	
+	
 .include "./Mode0Setup.asm"
+
+
