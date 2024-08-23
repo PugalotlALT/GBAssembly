53856e01ee2e948f74ca4af200558b4b1f2cbba2
PugalotlALT <aidan.prior2@education.nsw.gov.au>
2024-08-23 18:01:59 +1000

    attempted bug testing

--- MovementTest2/Addresses.asm
+++ MovementTest2/Addresses.asm
@@ -38,8 +38,8 @@ BGControlAddress:
 	.long 0x400000A
 	
 BGControl:
-	.int 0x0105
-	
+	.int 0x0104
+
 VBLankAddress:
 	.long 0x4000006
 	
@@ -56,7 +56,7 @@ TwoByTwoSprite:
 	.int 0b0100000000000000
 	
 SpritePriority:
-	.int 0b0000100000000000
+	.int 0b0000000000000000
 	
 RightScreenEdge:
 	.int 448
